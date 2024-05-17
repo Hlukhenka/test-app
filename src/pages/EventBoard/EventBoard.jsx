2232b44681ce96a3ae22847086e8592890c18850
Hlukhenka <anastasiagluhenka@gmail.com>
2024-05-17 15:25:30 +0300

    update

--- src/pages/EventBoard/EventBoard.jsx
+++ src/pages/EventBoard/EventBoard.jsx
@@ -4,7 +4,11 @@ import { Title } from './EventBoard.styled';
 import { allEvents } from '../../Redux/eventThunks';
 import { EventsList } from '../../components/EventsList/EventsList';
 import { Container } from '../../components/Container/Container';
-import { getAllEvents, getCurrentPage } from '../../Redux/selectors';
+import {
+  getAllEvents,
+  getCurrentPage,
+  getIsLoading,
+} from '../../Redux/selectors';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
 const EventBoard = () => {
@@ -12,6 +16,7 @@ const EventBoard = () => {
   const [hasMoreData, setHasMoreData] = useState(true);
   const dispatch = useDispatch();
   const currentPage = useSelector(getCurrentPage);
+  const isLoading = useSelector(getIsLoading);
   const events = useSelector(getAllEvents);
 
   const fetchMoreData = () => {
@@ -32,15 +37,20 @@ const EventBoard = () => {
   return (
     <Container>
       <Title>Events</Title>
-      <InfiniteScroll
-        next={fetchMoreData}
-        dataLength={events.length}
-        hasMore={true}
-        pageStart={page}
-        useWindow={false}
-      >
-        <EventsList />
-      </InfiniteScroll>
+
+      {isLoading ? (
+        <InfiniteScroll
+          next={fetchMoreData}
+          dataLength={events.length}
+          hasMore={true}
+          pageStart={page}
+          useWindow={false}
+        >
+          <EventsList />
+        </InfiniteScroll>
+      ) : (
+        <p>Loading...</p>
+      )}
     </Container>
   );
 };
